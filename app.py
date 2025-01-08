8533359551947ebedef4f0017b50aa1b522376c9
devfemibadmus <devfemibadmus@gmail.com>
2025-01-08 12:24:43 +0100

    👽 cors  🚸 config 🔽

--- app.py
+++ app.py
@@ -2,13 +2,18 @@ from datetime import datetime
 from flask import Flask, request, jsonify
 from models import cartype_bp, order_bp, admin_bp, db
 
-from flask_cors import CORS
-
 app = Flask(__name__)
-CORS(app, origins=["*","null"])
-app.register_blueprint(cartype_bp)
 app.register_blueprint(order_bp)
 app.register_blueprint(admin_bp)
+app.register_blueprint(cartype_bp)
+
+@app.after_request
+def after_request_func(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')  # Allow all origins
+    response.headers.add('Access-Control-Allow-Credentials', 'true')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type, x-csrf-token')
+    response.headers.add('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    return response
 
 @app.route('/<path:invalid_path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def catch_all(invalid_path):
